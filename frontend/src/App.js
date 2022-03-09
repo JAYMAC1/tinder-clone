7333734adbf7a292788ec844ac5d1d19a4a8d61b
James McNeil <mcneil_james@hotmail.com>
2022-03-09 20:50:59 +0000

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,11 @@
+// React related packages
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Custom pages and components
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Onboarding from './pages/Onboarding'
+
 const App = () => {
   return <div>App</div>
 }
