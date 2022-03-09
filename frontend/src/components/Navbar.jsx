552f4d3900b96d01bb9a27498754867b1e0ff8db
James McNeil <mcneil_james@hotmail.com>
2022-03-09 22:13:39 +0000

    Navber

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
-const Navbar = () => {
-  return <div>Navbar</div>
+// images
+import whiteLogo from '../images/white-tinder-logo.png'
+import colourLogo from '../images/colour-tinder-logo.png'
+
+const Navbar = ({ minimal, authToken }) => {
+  return (
+    <nav>
+      <div className='logo-container'>
+        <img
+          className='logo'
+          src={minimal ? colourLogo : whiteLogo}
+          alt='logo'
+        />
+      </div>
+      {!authToken && <button className='nav-button'>Log in</button>}
+    </nav>
+  )
 }
 
 export default Navbar
