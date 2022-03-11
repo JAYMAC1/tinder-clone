451503b7bb46b4c4d454e35cb0237010bad4890e
James McNeil <mcneil_james@hotmail.com>
2022-03-11 22:20:41 +0000

    starting to add form

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -4,12 +4,21 @@ const AuthModal = ({ setShowModal }) => {
   const handleClick = () => {
     setShowModal(false)
   }
+
+  const isSignUp = true
+
   return (
     <div className='auth-modal'>
       <div onClick={handleClick} className='close-icon'>
-        &#215;
+        X
       </div>
-      Auth Modal
+      <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque beatae
+        perspiciatis qui voluptatem cumque. Quidem tempore atque illum hic
+        excepturi, dicta laboriosam a cum nobis!
+      </p>
+      <form></form>
     </div>
   )
 }
