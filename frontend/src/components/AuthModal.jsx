a38eed5f7866c0fba72fe0055dac29e85bfea440
James McNeil <mcneil_james@hotmail.com>
2022-03-11 22:36:30 +0000

    complete signup form

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [error, setError] = useState(null)
   const handleClick = () => {
     setShowModal(false)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   const isSignUp = true
 
   return (
@@ -18,7 +25,37 @@ const AuthModal = ({ setShowModal }) => {
         perspiciatis qui voluptatem cumque. Quidem tempore atque illum hic
         excepturi, dicta laboriosam a cum nobis!
       </p>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          onChange={(e) => setEmail(e.targert.value)}
+          placeholder='Email'
+          required={true}
+          value={email}
+        />
+        <input
+          type='password'
+          id='password'
+          name='password'
+          onChange={(e) => setPassword(e.targert.value)}
+          placeholder='Password'
+          required={true}
+          value={password}
+        />
+        <input
+          type='password'
+          id='ConfirmPassword'
+          name='ConfirmPassword'
+          onChange={(e) => setConfirmPassword(e.targert.value)}
+          placeholder='ConfirmPassword'
+          required={true}
+          value={confirmPassword}
+        />
+        <input className='secondary-button' type='submit' value='Submit' />
+        <p>{error}</p>
+      </form>
     </div>
   )
 }
